4a2079b9a15b21b13a0d84d805f973f1160e6d5b
Emerson Silva Higino Filho <emersonfilho1@hotmail.com>
2022-08-19 21:03:51 -0300

    animacao ao scroll refeita

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
+import ScrollAnima from './modules/scroll-animacao.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
@@ -7,13 +7,8 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-<<<<<<< HEAD
-import initFetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-=======
 import fetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
->>>>>>> main
 
 //scrollsuave
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -33,20 +28,20 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 
 
 
 
-initAnimacaoScroll();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-<<<<<<< HEAD
-initFetchAnimais();
+
+
 fetchBitcoin('https://blockchain.info/ticker','.btc-preco');
 
-=======
-initFetchBitcoin();
 
 fetchAnimais('../../animaisapi.json','.numeros-grid');
->>>>>>> main
+

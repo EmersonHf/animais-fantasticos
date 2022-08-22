0aa73b11a818d3c84c314aa4b5aecc8fc39fa966
Emerson Silva Higino Filho <emersonfilho1@hotmail.com>
2022-08-22 15:22:43 -0300

    dropdown menu refatorado

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
@@ -32,10 +32,12 @@ const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
 
+const dropDownMenu = new DropdownMenu('[data-dropdown]');
+dropDownMenu.init();
 
 
 
-initDropdownMenu();
+
 initMenuMobile();
 initFuncionamento();
 
